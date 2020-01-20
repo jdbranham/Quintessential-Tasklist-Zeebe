c596ced246feea939358cab4793a72c79ae59af5
Jeremy Branham <Jeremy@savantly.net>
2020-01-19 20:19:47 -0600

    cleanup demo ui

--- src/main/kotlin/com/github/stephenott/qtz/ui/DefaultController.kt
+++ src/main/kotlin/com/github/stephenott/qtz/ui/DefaultController.kt
@@ -6,6 +6,7 @@ import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule
+import io.micronaut.security.token.jwt.validator.AuthenticationJWTClaimsSetAdapter
 import io.micronaut.security.utils.SecurityService
 import io.micronaut.views.View
 import java.security.Principal
@@ -22,24 +23,21 @@ class DefaultController(private val securityService: SecurityService?) {
     @View("index")
     @Get
     fun index(principal: Principal?): Map<String?, Any?>? {
+        var username = ""
         securityService?.authentication?.ifPresent {t: Authentication ->
-            println("${t.name}")
             println("${t.attributes}")
+            if (t is AuthenticationJWTClaimsSetAdapter) {
+                username = t.attributes["preferred_username"]?.toString() ?: t.attributes["sub"] as String
+            }
         }
         return mapOf(
             "oauthEnabled" to oauthEnabled,
             "loggedIn" to (principal != null),
-            "username" to principal?.name
+            "username" to username
         )
     }
 
-    @View("login")
-    @Get("/login/auth")
-    fun login(): Map<String?, Any?>? {
-        return HashMap()
-    }
-
-    @View("login")
+    @View("index")
     @Get("/login/authFailed")
     fun loginFailed(): Map<String?, Any?>? {
         return mapOf(
